6c9b12d8d8fd970755bdf571231f88247be5acab
Charlyn-Woodruff <scissorpaws7213@hotmail.com>
2026-03-28 00:07:51 +0000

    Update chat_support.php
    changed path to absolute

--- chat_support.php
+++ chat_support.php
@@ -17,7 +17,7 @@ $mainContent .= " <button style=\"  background-color: #555; color: white; paddin
   border: none; cursor: pointer; opacity: 0.8; position: absolute; bottom: 0px; right: 50px;
   width: 250px;\" onclick=\"openForm()\"> Chat</button> <div id=\"myChat\" style=\"display: none;
   position: absolute; bottom: -15px; right: 25px; border: 3px solid #055c1f; z-index: 9;\" > 
-  <form action=\"chat_action.php\" method=\"post\" style=\" max-width: 300px; padding: 10px;
+  <form action=\"/about/chat_action.php\" method=\"post\" style=\" max-width: 300px; padding: 10px;
   background-color: white;\"><h1>Chat</h1> <label for=\"msg\"><b>Message</b></label><label style=\"font-size:50px;float:right;\">&emsp;&emsp;&#128032;</label> <textarea style=\"  width: 25%;
   padding: 15px; width:100%; margin: 5px 0 22px 0; border: 1px solid black; background: #f1f1f1; resize: none;
   min-height: 200px; \" placeholder=\"Type message..\" name=\"msg\" id=\"msg\" required></textarea><button type=\"submit\" style=\"  background-color: #047048;
