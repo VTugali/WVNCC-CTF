c9ec5c3df67db33315cdd833e6367b4fb6248657
Charlyn-Woodruff <scissorpaws7213@hotmail.com>
2026-04-07 03:28:07 +0000

    Update yo_no_money.php to change format of image

--- banking/yo_no_money.php
+++ banking/yo_no_money.php
@@ -11,5 +11,5 @@ include "/var/www/html/banking/bankingFunctions.php";
 $mainContent = "";
 $mainContent .= "<div class=\"single-column\" role=\"presentation\">";
 $mainContent .= "<h2 style=\"margin-bottom:0;\">Thanks for choosing Northern Phish &amp; Loan!</h2><p style=\"color:red; font-size:1.5rem;\"><br> We can not process your transfer, not enough funds in your account!! Deposit some money fool!!</p></div>";
-$mainContent .= "<div style=\"margin-left:25%; margin-bottom:10%;\"><img src=\"/img/broke.gif\" width=\"500px\"; height=\"500px\";></div>"; 
+$mainContent .= "<div style=\"margin-left:25%; margin-bottom:10%;\"><img src=\"/img/broke.jpg\" width=\"500px\"; height=\"500px\";></div>"; 
 echo generatePage($mainContent);
