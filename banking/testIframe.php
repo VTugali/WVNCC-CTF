25d8bcfbbe58e7cbeef51da20dcbb78579e913c8
Charlyn-Woodruff <scissorpaws7213@hotmail.com>
2026-05-13 00:09:27 +0000

    Update testIframe.php

--- banking/testIframe.php
+++ banking/testIframe.php
@@ -13,7 +13,7 @@ include "/var/www/html/banking/bankingFunctions.php";
 $mainContent = "";
 
 $mainContent .= "<div style=\"background-color:#f0f8ff; margin:5% 0 0 0;\" >
-<p style=\"margin:15px;\">Embedded iframe Northern Phish &amp; Loan .</p>
+<p style=\"margin:15px;\">Embedded iframe Northern Phish &amp; Loan <br>Congrats! You have discovered our hidden fully functional website inside an iframe! <br> You receive a &#128681 for your hacking efforts </p>
 <iframe style=\"background-color:transparent;border:non; margin:5% 0 5% 5%;width:90%; height:85%;\" src=\"testIframe.php\">
 </iframe></div>";
 
